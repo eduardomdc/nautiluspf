c98b3e8aae3efd159056e8058bd10f6d9fa1d8a6
Eduardo Monteiro <eduardomonteirodacosta@gmail.com>
2023-03-17 12:01:58 -0300

    feat: receiveSonar

--- nemo_simulator/scripts/marlin.py
+++ nemo_simulator/scripts/marlin.py
@@ -1,25 +1,38 @@
 #!/usr/bin/env python3
 
 import rospy
-from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import PointStamped
 from geometry_msgs.msg import Twist
 
 class Marlin:
     def __init__(self):
         rospy.init_node('marlin', anonymous=True)
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+        rospy.Subscriber('/sonar_data', PointStamped, self.receiveSonar)
+        nemoPos = PointStamped()
 
-    def swim(self):    
+    def swim(self):
         rate = rospy.Rate(5)
-        msg = Twist()
-        msg.linear.x = 0
-        msg.linear.y = 1
-        msg.linear.z = 0
-        msg.angular.z = 1
-
+        velocity = Twist()
+        # teste = PointStamped()
+        # num = teste.point.x
+        num = nemoPos.point.x.data
+        velocity.linear.x = 1
         while not rospy.is_shutdown():
-            self.pub.publish(msg)
+            self.pub.publish(velocity)
             rate.sleep()
+    
+    def receiveSonar(self, msg):
+        # PointStamped:
+        #   Header:
+        #        uint32 seq
+        #        time stamp
+        #        string frame_id
+        #   Point:
+        #        x, y, z
+        
+        nemoPos = msg
+        rospy.loginfo(f"Sonar info: {nemoPos.point.x} {msg.point.y} {msg.point.z}")
 
 if __name__ == '__main__':
     marlin = Marlin()
