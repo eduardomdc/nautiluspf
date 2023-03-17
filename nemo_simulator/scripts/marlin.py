b2b548ed1484aed019e1bca3b798b3993d6fb219
Eduardo Monteiro <eduardomonteirodacosta@gmail.com>
2023-03-17 16:04:20 -0300

    basic steering

--- nemo_simulator/scripts/marlin.py
+++ nemo_simulator/scripts/marlin.py
@@ -28,75 +28,6 @@ cap.set(4,480)
 greenLower = (40, 40, 40)
 greenUpper = (70, 255, 255)
 
-while True:
-
-    success, imgOriginal = cap.read()
-
-    if success:
-        
-        #blur
-        blurred = cv2.GaussianBlur(imgOriginal, (11,11), 0)
-
-        #HSV
-        hsv = cv2.ctvColor(blurred, cv2.COLOR_BGR2HSV)
-        cv2.imshow("HSV IMAGE", hsv)
-
-        #mask for green
-        mask = cv2.inRange(hsv, greenLower, greenUpper)
-
-        #deleting noises which are in area of mark
-        mask = cv2.erode(mask, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
-        cv2.imshow("Mask + Erosion + Dilation", mask)
-
-        #countours
-        contours,_= cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,    cv2.CHAIN_APPROX_SIMPLE)
-        center = None
-
-        if len(contours) > 0:
-
-            #get max contour
-            c = max(contours, key=cv2.contourArea)
-
-            #return rectangle
-            rect = cv2.minAreaRect(c)
-            ((x,y), rotation) = rect
-
-            s = f"x: {np.round(x)}, y: {np.round(y)}, rotation: {np.round(rotation)}"
-            print(s)
-
-            #box
-            box = cv2.boxPoints(rect)
-            box = np.int64(box)
-
-            #moment
-            M = cv2.moments(c)
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-            #draw contour
-            cv2.drawContours(imgOriginal, [box], 0, (0, 255, 255), 2)
-
-            #point in center
-            cv2.circle(imgOriginal, center, 5, (255, 0, 255), -1)
-
-            # deque
-            pts.appendleft(center)
-            for i in range(1, len(pts)):
-
-                if pts[i - 1] is None or pts[i] is None: continue
-
-                cv2.line(imgOriginal, pts[i - 1], pts[i], (0, 255, 0), 3)
-
-        cv2.imshow("DETECTED IMAGE", imgOriginal)
-
-
-
-    if cv2.waitKey(1) & 0xFF == ord("q"): break
-
-
-
-
-
 class Marlin:
     def __init__(self):
         rospy.init_node('marlin', anonymous=True)
@@ -106,6 +37,7 @@ class Marlin:
         self.nemoPos = Point()
         self.nemoRealPos = Point()
         self.odomOrientation = Quaternion()
+        self.odomPosition = Point()
 
     def swim(self):
         rate = rospy.Rate(5)
@@ -113,12 +45,25 @@ class Marlin:
         # teste = PointStamped()
         # num = teste.point.x
         #num = self.nemoPos.point.x #nemoPos is undefinel for me
+        velocity.linear.y = 0.5
         while not rospy.is_shutdown():
             rospy.loginfo(f"Transformed sonar info: {self.nemoRealPos.x} {self.nemoRealPos.y} {self.nemoRealPos.z}")
-            if (self.nemoRealPos.y == 0):
-                velocity.angular.z = 1
+            self.steering(velocity)
             self.pub.publish(velocity)
             rate.sleep()
+
+    def steering(self, velocity):
+
+        #control change of direction
+        if (self.nemoRealPos.y > 0):
+            if (self.nemoRealPos.x < 0):
+                velocity.angular.z = -1
+            else:
+                velocity.angular.z = 1
+        else:
+            velocity.angular.z = 2
+
+        #if position is the same, reverse
 
     def transform(self, orientation, absPos):
         # we need to rotate the nemoPos to account for the orientation
@@ -139,13 +84,9 @@ class Marlin:
     
     def receiveOdom(self, msg):
         self.odomOrientation = msg.pose.pose.orientation
+        self.odomPosition = msg.pose.pose.position
 
 
 if __name__ == '__main__':
     marlin = Marlin()
     marlin.swim()
-
-
-
-
-
