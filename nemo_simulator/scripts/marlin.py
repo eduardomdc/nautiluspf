3751d8bcfcd111b321da534485c4e83ee8daee98
Eduardo Monteiro <eduardomonteirodacosta@gmail.com>
2023-03-17 14:38:40 -0300

    receiveOdom

--- nemo_simulator/scripts/marlin.py
+++ nemo_simulator/scripts/marlin.py
@@ -2,22 +2,28 @@
 
 import rospy
 from geometry_msgs.msg import PointStamped
+from geometry_msgs.msg import Point
 from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Quaternion
+from nav_msgs.msg import Odometry
 
 class Marlin:
     def __init__(self):
         rospy.init_node('marlin', anonymous=True)
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         rospy.Subscriber('/sonar_data', PointStamped, self.receiveSonar)
-        nemoPos = PointStamped()
+        rospy.Subscriber('/odom', Odometry, self.receiveOdom)
+        nemoPos = Point()
+        odomOrientation = Quaternion()
 
     def swim(self):
         rate = rospy.Rate(5)
         velocity = Twist()
         # teste = PointStamped()
         # num = teste.point.x
-        num = nemoPos.point.x.data #nemoPos is undefinel for me
-        velocity.linear.x = 1
+        #num = self.nemoPos.point.x #nemoPos is undefinel for me
+        velocity.linear.y = 0.5 
         while not rospy.is_shutdown():
             self.pub.publish(velocity)
             rate.sleep()
@@ -31,8 +37,12 @@ class Marlin:
         #   Point:
         #        x, y, z
         
-        nemoPos = msg
-        rospy.loginfo(f"Sonar info: {nemoPos.point.x} {msg.point.y} {msg.point.z}")
+        nemoPos = msg.point
+        rospy.loginfo(f"Sonar info: {nemoPos.x} {msg.point.y} {msg.point.z}")
+    
+    def receiveOdom(self, msg):
+        odomOrientation = msg.pose.pose.orientation
+
 
 if __name__ == '__main__':
     marlin = Marlin()
