19df6dd6ade4febbab9ceeb831ccccf0508f52a5
Eduardo Monteiro <eduardomonteirodacosta@gmail.com>
2023-03-17 15:12:33 -0300

    reference frame transform

--- nemo_simulator/scripts/marlin.py
+++ nemo_simulator/scripts/marlin.py
@@ -7,6 +7,9 @@ from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
 from geometry_msgs.msg import Quaternion
 from nav_msgs.msg import Odometry
+from tf.transformations import euler_from_quaternion
+import math
+import numpy as np
 
 class Marlin:
     def __init__(self):
@@ -14,8 +17,9 @@ class Marlin:
         self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         rospy.Subscriber('/sonar_data', PointStamped, self.receiveSonar)
         rospy.Subscriber('/odom', Odometry, self.receiveOdom)
-        nemoPos = Point()
-        odomOrientation = Quaternion()
+        self.nemoPos = Point()
+        self.nemoRealPos = Point()
+        self.odomOrientation = Quaternion()
 
     def swim(self):
         rate = rospy.Rate(5)
@@ -23,25 +27,30 @@ class Marlin:
         # teste = PointStamped()
         # num = teste.point.x
         #num = self.nemoPos.point.x #nemoPos is undefinel for me
-        velocity.linear.y = 0.5 
+        velocity.angular.z = 1
         while not rospy.is_shutdown():
             self.pub.publish(velocity)
             rate.sleep()
-    
+
+    def transform(self, orientation, absPos):
+        # we need to rotate the nemoPos to account for the orientation
+        # of the robot around the Z axis
+        orientation_list = [orientation.x, orientation.y, orientation.z, orientation.w]
+        (roll, pitch, yaw) = euler_from_quaternion(orientation_list)
+        #rotation matrix for Z-axis rotation
+        rotationMatrix = np.array([[math.cos(yaw), math.sin(yaw)],
+                                   [-math.sin(yaw), math.cos(yaw)]])
+        (self.nemoRealPos.x, self.nemoRealPos.y) = np.dot(rotationMatrix, [self.nemoPos.x, self.nemoPos.y])
+        self.nemoRealPos.y = -self.nemoRealPos.y # +y is in front
+
     def receiveSonar(self, msg):
-        # PointStamped:
-        #   Header:
-        #        uint32 seq
-        #        time stamp
-        #        string frame_id
-        #   Point:
-        #        x, y, z
-        
-        nemoPos = msg.point
-        rospy.loginfo(f"Sonar info: {nemoPos.x} {msg.point.y} {msg.point.z}")
+        self.nemoPos = msg.point
+        rospy.loginfo(f"Sonar info: {msg.point.x} {msg.point.y} {msg.point.z}")
+        self.transform(self.odomOrientation, self.nemoPos)
+        rospy.loginfo(f"Transformed sonar info: {self.nemoRealPos.x} {self.nemoRealPos.y} {self.nemoRealPos.z}")
     
     def receiveOdom(self, msg):
-        odomOrientation = msg.pose.pose.orientation
+        self.odomOrientation = msg.pose.pose.orientation
 
 
 if __name__ == '__main__':
